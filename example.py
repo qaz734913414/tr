9421ce53bd01aaca60556ed2604811e8e288d0ad
wilab <anycode#yahoo.com>
2019-09-19 21:22:38 +0800

    new

--- example.py
+++ example.py
@@ -9,5 +9,8 @@ print(tr.detect("imgs/id_card.jpeg", tr.FLAG_ROTATED_RECT))
 # recognize text line, return (text, confidence)
 print(tr.recognize("imgs/line.png"))
 
-# detect and recognize, return list of ((x, y, width, height), text, confidence)
+# detect and recognize text lines, return list of ((x, y, width, height), text, confidence)
 print(tr.run("imgs/name_card.jpg"))
+
+# detect and recognize text lines with angle, return list of ((cx, cy, width, height, angle), text, confidence)
+print(tr.run_angle("imgs/id_card.jpeg"))
